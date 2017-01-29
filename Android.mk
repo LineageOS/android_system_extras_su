f73434ad99cb8245b4d9cee89cce449f965449a7
Jason Riordan <jriordan001@gmail.com>
2017-01-28 22:56:28 -0500

    su: move contents of init.superuser.rc to init.cm.rc
    In cm-13.0 all rc files are in ramdisk and not easily changed.
    This allows root to be disabled by default in cm-13.0 branch
    and the service defines to forever be present when su is not built.
    
    Change-Id: I73bbf37e547323846cd863b3dcea5c890f9ee969

--- Android.mk
+++ Android.mk
@@ -20,15 +20,8 @@ $(SYMLINKS):
 	@rm -rf $@
 	$(hide) ln -sf ../xbin/su $@
 
-# make sure init.superuser.rc is imported from
-# init.rc or similar
-
-SUPERUSER_RC := $(TARGET_ROOT_OUT)/init.superuser.rc
-$(SUPERUSER_RC): $(LOCAL_PATH)/init.superuser.rc | $(ACP)
-	$(copy-file-to-new-target)
-
 # We need this so that the installed files could be picked up based on the
 # local module name
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
-    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS) $(SUPERUSER_RC)
+    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
 
