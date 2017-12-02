3193b618a317576f11bcaf3de5f8f638d3584dbb
MSe1969 <mse1969@posteo.de>
2017-12-02 22:31:38 +0100

    Add -lstdc++ flag to linker to avoid below error: undefined reference to 'operator new(unsigned int)'
    Change-Id: I3bc3010cbb42d5b177c4d13e8e8aa98e26ee7492

--- Android.mk
+++ Android.mk
@@ -11,6 +11,7 @@ LOCAL_WHOLE_STATIC_LIBRARIES := libc libcutils libutils libbinder liblog
 LOCAL_SRC_FILES := su.c daemon.c utils.c pts.c
 LOCAL_SRC_FILES += binder/appops-wrapper.cpp binder/pm-wrapper.c
 LOCAL_CFLAGS += -Werror
+LOCAL_LDFLAGS += -lstdc++
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 include $(BUILD_EXECUTABLE)
 
